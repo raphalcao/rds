7d4f6bc52703c51cd0b30682790b58b8652da4b4
Raphael Falcao <raphael.falcao@febrafar.com.br>
2025-02-11 15:17:16 -0300

    feat/adjust_path

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -5,9 +5,36 @@ data "aws_db_subnet_group" "existing_subnet_group" {
 
 # 🏗️ Cria o DB Subnet Group apenas se não existir
 resource "aws_db_subnet_group" "rds_subnet_group" {
-  count      = length(try(data.aws_db_subnet_group.existing_subnet_group.id, [])) > 0 ? 0 : 1
+  count      = length(try([data.aws_db_subnet_group.existing_subnet_group.id], [])) > 0 ? 0 : 1
   name       = "rds-subnet-group"
   subnet_ids = var.private_subnets
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# 🔍 Verifica se o Security Group já existe
+data "aws_security_group" "existing_sg" {
+  filter {
+    name   = "group-name"
+    values = ["rds-sg"]
+  }
+}
+
+# 🏗️ Cria o Security Group apenas se não existir
+resource "aws_security_group" "rds_sg" {
+  count       = length(try([data.aws_security_group.existing_sg.id], [])) > 0 ? 0 : 1
+  name        = "rds-sg"
+  description = "Security Group para RDS MySQL"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   lifecycle {
     prevent_destroy = true
@@ -26,13 +53,13 @@ resource "aws_db_instance" "rds_mysql" {
   password             = var.db_password
   publicly_accessible   = false
 
-  db_subnet_group_name = length(try(data.aws_db_subnet_group.existing_subnet_group.id, "")) > 0 
-                      ? data.aws_db_subnet_group.existing_subnet_group.id 
+  db_subnet_group_name = length(try([data.aws_db_subnet_group.existing_subnet_group.id], [])) > 0
+                      ? data.aws_db_subnet_group.existing_subnet_group.id
                       : aws_db_subnet_group.rds_subnet_group[0].id
 
-  vpc_security_group_ids = length(try(data.aws_security_group.existing_sg.id, "")) > 0 
-                        ? [data.aws_security_group.existing_sg.id] 
-                        : [aws_security_group.rds_sg[0].id]
+  vpc_security_group_ids = length(try([data.aws_security_group.existing_sg.id], [])) > 0
+                        ? [data.aws_security_group.existing_sg.id]
+                        : aws_security_group.rds_sg[*].id
 
   skip_final_snapshot   = true
 }
