b7c28c599e5420658908e04526f975ad4ef1d4bd
Raphael Falcao <raphael.falcao@febrafar.com.br>
2025-02-11 16:06:04 -0300

    db_subnet_group_name

--- terraform/security-group.tf
+++ terraform/security-group.tf
@@ -8,14 +8,14 @@ resource "aws_security_group" "rds_sg" {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # ⚠️ Substitua pelo IP da sua aplicação
+    cidr_blocks = ["172.31.16.0/20"]  # ⚠️ Substitua pelo IP da sua aplicação
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["172.31.16.0/20"]
   }
 
   lifecycle {
