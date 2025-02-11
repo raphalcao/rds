e6c99f6d2e2c576dd3d55efbca2827bcf358827d
Raphael Falcao <raphael.falcao@febrafar.com.br>
2025-02-11 15:13:10 -0300

    feat/adjust_path

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -27,12 +27,12 @@ resource "aws_db_instance" "rds_mysql" {
   publicly_accessible   = false
 
   db_subnet_group_name = length(try(data.aws_db_subnet_group.existing_subnet_group.id, "")) > 0 
-                        ? data.aws_db_subnet_group.existing_subnet_group.id 
-                        : aws_db_subnet_group.rds_subnet_group[0].id
+                      ? data.aws_db_subnet_group.existing_subnet_group.id 
+                      : aws_db_subnet_group.rds_subnet_group[0].id
 
   vpc_security_group_ids = length(try(data.aws_security_group.existing_sg.id, "")) > 0 
-                          ? [data.aws_security_group.existing_sg.id] 
-                          : [aws_security_group.rds_sg[0].id]
+                        ? [data.aws_security_group.existing_sg.id] 
+                        : [aws_security_group.rds_sg[0].id]
 
   skip_final_snapshot   = true
 }
